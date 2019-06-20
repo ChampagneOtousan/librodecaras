eb0b30d6f77c42eda9bbc8f324212a2041d08330
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-06-20 16:38:40 -0400

    framework to start working on bookface profile

---
+++ server.rb
@@ -0,0 +1,26 @@
+require "sinatra"
+require "faker"
+
+get "/" do
+  erb :home
+end
+
+get "/" do
+  erb :home
+end
+
+get "/about" do
+  erb :about
+end
+
+get "/contact" do
+  erb :contact
+end
+
+get "/friends" do
+  erb :friends
+end
+
+get "/skills" do
+  erb :skills
+end
